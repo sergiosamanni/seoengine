cb4673fc6c04e77c541ec17edd3aa41a3602dbe1
SamaS-81 <freedomlancers.sergio@gmail.com>
2026-03-26 15:46:05 +0100

    Fix: Added ca-certificates and used certifi to resolve MongoDB SSL handshake issues.

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,9 @@
+import certifi
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb://localhost:27017")
 DB_NAME = os.environ.get("DB_NAME", "seoengine")
 
-client = AsyncIOMotorClient(MONGO_URL)
+client = AsyncIOMotorClient(MONGO_URL, tlsCAFile=certifi.where())
 db = client[DB_NAME]
